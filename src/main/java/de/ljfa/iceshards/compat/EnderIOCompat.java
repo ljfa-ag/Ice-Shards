17aa9d40befd32a662af90462b3907e28c0320bf
ljfa-ag <ljfa-ag@web.de>
2015-04-07 15:46:52 +0200

    Add SAG Mill recipe for ice shards -> snowballs

--- src/main/java/de/ljfa/iceshards/compat/EnderIOCompat.java
+++ src/main/java/de/ljfa/iceshards/compat/EnderIOCompat.java
@@ -5,9 +5,9 @@ import cpw.mods.fml.common.event.FMLInterModComms;
 public class EnderIOCompat {
 
     public static void addSAGMillRecipes() {
-        //SAG Mill: ice and paced ice -> ice shards
         FMLInterModComms.sendMessage("EnderIO", "recipe:sagmill",
             "<recipeGroup name=\"GlassShards\">" +
+              //SAG Mill: ice -> ice shards
               "<recipe name=\"Ice\" energyCost=\"1000\">" +
                 "<input>" +
                   "<itemStack modID=\"minecraft\" itemName=\"ice\" />" +
@@ -16,12 +16,22 @@ public class EnderIOCompat {
                   "<itemStack modID=\"ice_shards\" itemName=\"ice_shards\" />" +
                 "</output>" +
               "</recipe>" +
+              //SAG Mill: packed ice -> ice shards
               "<recipe name=\"Packed Ice\" energyCost=\"1000\">" +
                 "<input>" +
                   "<itemStack modID=\"minecraft\" itemName=\"packed_ice\" />" +
                 "</input>" +
                 "<output>" +
                   "<itemStack modID=\"ice_shards\" itemName=\"ice_shards\" />" +
+                "</output>" +
+              "</recipe>" +
+              //SAG Mill: ice shards -> snow
+              "<recipe name=\"Ice Shards\" energyCost=\"800\">" +
+                "<input>" +
+                  "<itemStack oreDictionary=\"shardsIce\" />" +
+                "</input>" +
+                "<output>" +
+                  "<itemStack modID=\"minecraft\" itemName=\"snowball\" number=\"2\" />" +
                 "</output>" +
               "</recipe>" +
             "</recipeGroup>");
