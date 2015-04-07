5b0e582a63ad63db02b2b419d966b4d38b39f274
ljfa-ag <ljfa-ag@web.de>
2015-04-07 15:48:26 +0200

    Add pulverizer recipe for ice shards -> snowballs

--- src/main/java/de/ljfa/iceshards/compat/ThermalExpCompat.java
+++ src/main/java/de/ljfa/iceshards/compat/ThermalExpCompat.java
@@ -1,6 +1,7 @@
 package de.ljfa.iceshards.compat;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
@@ -17,6 +18,7 @@ public class ThermalExpCompat {
     public static void addRecipes() {
         ThermalExpansionHelper.addPulverizerRecipe(2800, new ItemStack(Blocks.ice), new ItemStack(ModItems.ice_shards));
         ThermalExpansionHelper.addPulverizerRecipe(2800, new ItemStack(Blocks.packed_ice), new ItemStack(ModItems.ice_shards));
+        ThermalExpansionHelper.addPulverizerRecipe(2000, new ItemStack(ModItems.ice_shards), new ItemStack(Items.snowball, 2));
         
         ThermalExpansionHelper.addCrucibleRecipe(1400, new ItemStack(ModItems.ice_shards), new FluidStack(FluidRegistry.WATER, 1000));
         
