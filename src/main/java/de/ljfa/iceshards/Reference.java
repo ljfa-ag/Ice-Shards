78af9ad376759f9f27af34e50572ce8fb54349ec
ljfa-ag <ljfa-ag@web.de>
2015-05-24 01:18:44 +0200

    Use ${version} instead of @VERSION@ for the Reference

--- src/main/java/de/ljfa/iceshards/Reference.java
+++ src/main/java/de/ljfa/iceshards/Reference.java
@@ -3,6 +3,6 @@ package de.ljfa.iceshards;
 public class Reference {
     public static final String MODID = "ice_shards";
     public static final String MODNAME = "Ice Shards";
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "${version}";
     public static final String GUI_FACTORY_CLASS = "de.ljfa.iceshards.gui.IsGuiFactory";
 }
