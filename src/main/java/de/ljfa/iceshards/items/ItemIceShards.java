273e66d42591a68fe766beca05cceaf5d5adae1a
ljfa-ag <ljfa-ag@web.de>
2015-05-24 10:50:39 +0200

    Fix access modifiers (cherry picked from commit 146d89542858a26b17ad938db01ad6c71dd90c7a)
    Conflicts:
    	src/main/java/de/ljfa/iceshards/items/ItemIceShards.java

--- src/main/java/de/ljfa/iceshards/items/ItemIceShards.java
+++ src/main/java/de/ljfa/iceshards/items/ItemIceShards.java
@@ -1,20 +1,17 @@
 package de.ljfa.iceshards.items;
 
 import ljfa.glassshards.items.IModeledItem;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemModelMesher;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import de.ljfa.iceshards.Reference;
 
 public class ItemIceShards extends Item implements IModeledItem {
-    public static String name = "ice_shards";
+    public static final String name = "ice_shards";
     
-    ItemIceShards() {
+    public ItemIceShards() {
         setCreativeTab(CreativeTabs.tabMaterials);
         setUnlocalizedName(Reference.MODID + ":" + name);
         GameRegistry.registerItem(this, name);
