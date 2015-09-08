f2a07e4ddd4a6a158c5b09abfb557353dbce3f90
ljfa-ag <ljfa-ag@web.de>
2015-09-08 18:02:10 +0200

    Put EnderIO recipe XMLs into a resource file

--- src/main/java/de/ljfa/iceshards/compat/EnderIOCompat.java
+++ src/main/java/de/ljfa/iceshards/compat/EnderIOCompat.java
@@ -1,39 +1,23 @@
 package de.ljfa.iceshards.compat;
 
+import java.io.IOException;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
 import cpw.mods.fml.common.event.FMLInterModComms;
 
 public class EnderIOCompat {
 
     public static void addSAGMillRecipes() {
-        FMLInterModComms.sendMessage("EnderIO", "recipe:sagmill",
-            "<recipeGroup name=\"GlassShards\">" +
-              //SAG Mill: ice -> ice shards
-              "<recipe name=\"Ice\" energyCost=\"1000\">" +
-                "<input>" +
-                  "<itemStack modID=\"minecraft\" itemName=\"ice\" />" +
-                "</input>" +
-                "<output>" +
-                  "<itemStack modID=\"ice_shards\" itemName=\"ice_shards\" />" +
-                "</output>" +
-              "</recipe>" +
-              //SAG Mill: packed ice -> ice shards
-              "<recipe name=\"Packed Ice\" energyCost=\"1000\">" +
-                "<input>" +
-                  "<itemStack modID=\"minecraft\" itemName=\"packed_ice\" />" +
-                "</input>" +
-                "<output>" +
-                  "<itemStack modID=\"ice_shards\" itemName=\"ice_shards\" />" +
-                "</output>" +
-              "</recipe>" +
-              //SAG Mill: ice shards -> snow
-              "<recipe name=\"Ice Shards\" energyCost=\"800\">" +
-                "<input>" +
-                  "<itemStack oreDictionary=\"shardsIce\" />" +
-                "</input>" +
-                "<output>" +
-                  "<itemStack modID=\"minecraft\" itemName=\"snowball\" number=\"2\" />" +
-                "</output>" +
-              "</recipe>" +
-            "</recipeGroup>");
+        try {
+            FMLInterModComms.sendMessage("EnderIO", "recipe:sagmill",
+                "<recipeGroup name=\"GlassShards\">"
+                + Resources.toString(Resources.getResource("assets/ice_shards/EnderIORecipes/SagRecipes.xml"), Charsets.UTF_8)
+                + "</recipeGroup>");
+        }
+        catch(IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
