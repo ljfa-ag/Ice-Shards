9dc3afc5627a327a7d2e61110fe24eef01d43509
ljfa-ag <ljfa-ag@users.noreply.github.com>
2017-03-07 12:38:58 +0100

    Version bump

--- src/main/java/de/ljfa/iceshards/IceShards.java
+++ src/main/java/de/ljfa/iceshards/IceShards.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION,
-    dependencies = "required-after:glass_shards@[1.6.2,),[${version}]", guiFactory = Reference.GUI_FACTORY_CLASS,
+    dependencies = "required-after:glass_shards@[1.6.3,),[${version}]", guiFactory = Reference.GUI_FACTORY_CLASS,
     acceptedMinecraftVersions = "[1.11.2,)", updateJSON = Reference.UPDATE_JSON)
 public class IceShards {
     @Mod.Instance(Reference.MODID)
